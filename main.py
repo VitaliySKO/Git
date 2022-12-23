b7fe145c9bfe8162be58dd2ce62bd7b508f3da4d
Vitaliy <vsssv@ya.ru>
2022-12-23 19:04:24 +0300

    Commit 4

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Hello")
+a = str(input("Input text:"))
+text = a.split(" ")
+dict_text = {}
+for word in text:
+    word = word.lower()
+    if word in dict_text:
+        dict_text[word] += 1
+    else:
+        dict_text[word] = 1
+
+for key in dict_text:
+    print(key, dict_text[key])
